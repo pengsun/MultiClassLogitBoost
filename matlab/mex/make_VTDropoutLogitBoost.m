9af568eb0a636004de678a18d772a0d40bb43682
pengsun <sunp08@mails.tsinghua.edu.cn>
2013-07-21 18:18:15 +0800

    make_VTDropoutLogitBoost.m
    Signed-off-by: pengsun <sunp08@mails.tsinghua.edu.cn>

--- matlab/mex/make_VTDropoutLogitBoost.m
+++ matlab/mex/make_VTDropoutLogitBoost.m
@@ -1,7 +1,7 @@
 %%
 % settings
-settings_labpc
-% settings_labserver
+% settings_labpc
+settings_labserver
 %%
 name = 'VTDropoutLogitBoost_mex';
 fn = {...
@@ -11,18 +11,18 @@ fn = {...
   fullfile(dir_src,'VTDropoutLogitBoost.cpp')
   };
 %% Debug
-% outnamed = sprintf('-output %s', [name,'d']);
-% cmdd = sprintf('mex -g %s %s %s %s',...
-%   fn{:});
-% cmdd = sprintf('%s %s',...
-%   cmdd, opt_cmdd);
-% eval(cmdd);
-% copyfile([name,'.',mexext], './../private/');
-%% Release
-outname = sprintf('-output %s', name);
-cmd = sprintf('mex -O %s %s %s %s',...
+outnamed = sprintf('-output %s', [name,'d']);
+cmdd = sprintf('mex -g %s %s %s %s',...
   fn{:});
-cmd = sprintf('%s %s',...
-  cmd, opt_cmd);
-eval(cmd);
+cmdd = sprintf('%s %s',...
+  cmdd, opt_cmdd);
+eval(cmdd);
 copyfile([name,'.',mexext], './../private/');
+%% Release
+% outname = sprintf('-output %s', name);
+% cmd = sprintf('mex -O %s %s %s %s',...
+%   fn{:});
+% cmd = sprintf('%s %s',...
+%   cmd, opt_cmd);
+% eval(cmd);
+% copyfile([name,'.',mexext], './../private/');
