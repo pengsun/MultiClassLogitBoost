ef607742c223ea5fa0176a2744234208baa7a5c4
pengsun <sunp08@mails.tsinghua.edu.cn>
2013-07-24 09:48:12 +0800

    minor modification to pVTLogitBoost_run.m
    Signed-off-by: pengsun <sunp08@mails.tsinghua.edu.cn>

--- matlab/script_run/pVTLogitBoost_run.m
+++ matlab/script_run/pVTLogitBoost_run.m
@@ -1,12 +1,16 @@
 %% 
-name = 'pendigits';
-fn_data = fullfile('.\dataset',[name,'.mat']);
-dir_rst = fullfile('.\rst\pVTLogitBoost',name);
+name = 'M-Image';
+fn_data = fullfile('E:\Users\sp\dataset_mat',[name,'.mat']);
+dir_rst = fullfile('E:\Users\sp\code_work\gitlab\MulticlassLogitBoost\matlab\rst\pVTLogitBoost',name);
+
+% name = 'pendigits';
+% fn_data = fullfile('.\dataset',[name,'.mat']);
+% dir_rst = fullfile('.\rst\pVTLogitBoost',name);
 %%
 num_Tpre = 2000;
-T = 700;
-cv  = {0.1};
-cJ = {20};
+T = 10000;
+cv  = {0.4};
+cJ = {50};
 cns = {1};
 %%
 h = batch_pVTLogitBoost();
