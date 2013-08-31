a651356aa4c9303699fbbcfa5070df902476461b
pengsun <sunp08@mails.tsinghua.edu.cn>
2013-09-01 01:41:38 +0800

    modify batch_sampboost_basic.m (number precision) modify main_comp_2algo.m (print training time)

--- matlab/main_comp_2algo.m
+++ matlab/main_comp_2algo.m
@@ -1,20 +1,21 @@
 %% data name
 % name = 'poker100k';
 % name = 'pendigits';
-name = 'optdigits';
+% name = 'optdigits';
 % name = 'M-Basic';
 % name = 'M-Image';
 % name = 'isolet';
 % name = 'zipcode';
+name = 'mnist';
 %% algo name
 algoname1 = 'pExtSampVTLogitBoost';
 dir_root1 = fullfile('.\rst',algoname1);
-fn1 = 'T1500_v1.0e-01_J8_ns1_rs1_rf6e-02_rc1.mat';
+fn1 = 'T5000_v1.0e-01_J50_ns1_rs1_rf6e-03_rc1.mat';
 % fn1 = 'T700_v1_J20_ns1.mat';
 
-algoname2 = 'pSampVTLogitBoost';
+algoname2 = 'pExtSampVTLogitBoost';
 dir_root2 = fullfile('.\rst',algoname2);
-fn2 = 'T1500_v1.0e-01_J8_ns1_rs1_rf6e-02_rc1.mat';
+fn2 = 'T5000_v1.0e-01_J50_ns1_rs1_rf2e-03_rc1.mat';
 
 % algoname1 = 'VTLogitBoost';
 % dir_root1 = fullfile('.\rst',algoname1);
@@ -33,6 +34,7 @@ abs_grad1 = tmp.abs_grad;
 % abs_grad1 = tmp.loss_tr;
 % F1 = tmp.F;
 num_it1 = tmp.num_it;
+time_tr1 = tmp.time_tr;
 clear tmp;
 
 ffn2 = fullfile(dir_root2,name,fn2);
@@ -42,7 +44,7 @@ err_it2 = tmp.err_it;
 abs_grad2 = tmp.abs_grad;
 % F2 = tmp.F;
 num_it2 = tmp.num_it;
-
+time_tr2 = tmp.time_tr;
 %% plot error
 figure('name',name); title error; hold on;
 % plot(it1,err_it1, 'color','r','marker','.');
@@ -67,11 +69,17 @@ fprintf('best result:\n');
 [err2best,it2best] = min(err_it2);
 fprintf('%s: %d @ %d\n', algoname1, err1best, it1(it1best) );
 fprintf('%s: %d @ %d\n', algoname2, err2best, it2(it2best));
-
+fprintf('\n');
 %% print last result
 fprintf('last result:\n');
 fprintf('%s: %d @ %d\n', algoname1, err_it1(end), it1(end));
 fprintf('%s: %d @ %d\n', algoname2, err_it2(end), it2(end));
+fprintf('\n');
+%% print training time
+fprintf('training time:\n');
+fprintf('%s: %d \n', algoname1, time_tr1);
+fprintf('%s: %d \n', algoname2, time_tr2);
+fprintf('\n');
 %% plot grad
 figure('name',name);  title grad; hold on;
 plot(it1, log10( eps + abs_grad1(it1) ), 'color','r','marker','.');
