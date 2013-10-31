8fdd58ccdde29a0a9f1410e12f325e2c3137cb3a
pengsun <sunp08@mails.tsinghua.edu.cn>
2013-10-31 14:28:43 +0800

    pAOSOLogitBoostV2

---
+++ matlab/script_run/pAOSOLogitBoostV2.m
@@ -0,0 +1,31 @@
+%%
+name = 'optdigits';
+dir_data = 'D:\Data\dataset_mat';
+fn_data = fullfile(dir_data, [name,'.mat']);
+dir_rst = fullfile('.\',...
+  'rst\pAOSOLogitBoostV2',name);
+%%
+num_Tpre = 5000;
+T = 5000;
+cv  = {0.1};
+cJ = {20};
+cns = {1};
+%%% sample
+crs = {0.1};
+cwrs = {0.95};
+%%% feature
+crf = {0.2};
+%%
+h = batch_pAOSOLogitBoostV2();
+h.num_Tpre = num_Tpre;
+h.T = T;
+h.cv = cv;
+h.cJ = cJ;
+h.cns = cns;
+% sample
+h.cwrs = cwrs;
+h.crs = crs;
+% feature
+h.crf = crf;
+run_all_param(h, fn_data, dir_rst);
+clear h;
