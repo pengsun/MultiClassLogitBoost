45bc915dffeff236c553e42b45912f5c5889c03b
pengsun <pengsun.thu@gmail.com>
2013-11-06 20:57:45 +0800

    minor modification to matlab scripts

--- matlab/main_comp_2algo_nop_err.m
+++ matlab/main_comp_2algo_nop_err.m
@@ -1,13 +1,13 @@
 %% config
-name = 'zipcode';
+name = 'isolet';
 
 algoname1 = 'pCoSamp2VTLogitBoost';
 dir_root1 = fullfile('.\rst',algoname1);
-fn1 = 'T5000_v1.0e-01_J20_ns1_rf5.00e-02_rb5.00e-02_wrb9.50e-01.mat';
+fn1 = 'T5000_v1.0e-01_J20_ns1_rf5.14e-02_rb5.00e-02_wrb9.50e-01.mat';
 
 algoname2 = 'pVbExtSamp14VTLogitBoost';
 dir_root2 = fullfile('.\rst',algoname2);
-fn2 = 'T5000_v1.0e-01_J20_ns1_wrs9.50e-01_rs5.00e-02_rf5.00e-02_wrc1.10e+00_rc1.10e+00.mat';
+fn2 = 'T5000_v1.0e-01_J20_ns1_wrs9.50e-01_rs5.00e-02_rf5.14e-02_wrc1.10e+00_rc1.10e+00.mat';
 
 %% load
 ffn1 = fullfile(dir_root1,name,fn1);
--- matlab/main_view_VbExtSamp5_InstCls.m
+++ matlab/main_view_VbExtSamp5_InstCls.m
@@ -1,8 +1,8 @@
 %% config
-name = 'optdigits';
-algoname1 = 'pVbExtSamp13VTLogitBoost_rs0.01';
+name = 'isolet';
+algoname1 = 'pVbExtSamp14VTLogitBoost_tmp';
 dir_root1 = fullfile('.\rst',algoname1);
-fn1 = 'T5000_v1.0e-001_J20_ns1_wrs1.10e+000_rs1.00e-002_rf1.10e+000_wrc1.10e+000_rc1.10e+000.mat';
+fn1 = 'T5000_v1.0e-01_J20_ns1_wrs9.00e-01_rs1.10e+00_rf5.14e-02_wrc1.10e+00_rc1.10e+00.mat';
 %% load
 ffn1 = fullfile(dir_root1,name,fn1);
 tmp = load(ffn1);
@@ -17,24 +17,7 @@ time_tr1 = tmp.time_tr;
 tree_node_cc = tmp.tree_node_cc;
 tree_node_sc = tmp.tree_node_sc;
 clear tmp;
-%% class Max Min
-for i = 1 : numel(tree_node_cc)
-  Mc(i) = max( tree_node_cc{i} );
-  mc(i) = min( tree_node_cc{i} );
-end
-figure;
-plot(it1, mc, it1, Mc, 'marker','x');
-legend('minCls','MAXCls');
-grid on;
-%% class root node
-for i = 1 : numel(tree_node_cc)
-  cc(i) = tree_node_cc{i}(1);
-end
-figure;
-plot(it1, cc, 'marker','x');
-title('Cls Count, root node');
-grid on;
-%% examples & class
+%% print examples & class
 for i = 1 : numel(tree_node_sc)
   sc(i) = tree_node_sc{i}(1);
 end
@@ -46,6 +29,28 @@ navg = mean(sc);
 cavg = mean(cc);
 fprintf(name);fprintf('\n');
 fprintf('avg examples = %d\n',navg);
+%% plot avg examples
+figure('name','avg examples');
+plot(it1, sc, 'marker','x');
+set(gca,'xscale','log');
+%% plot class Max Min
+for i = 1 : numel(tree_node_cc)
+  Mc(i) = max( tree_node_cc{i} );
+  mc(i) = min( tree_node_cc{i} );
+end
+figure;
+plot(it1, mc, it1, Mc, 'marker','x');
+legend('minCls','MAXCls');
+grid on;
+%% plot class root node
+for i = 1 : numel(tree_node_cc)
+  cc(i) = tree_node_cc{i}(1);
+end
+figure;
+plot(it1, cc, 'marker','x');
+title('Cls Count, root node');
+grid on;
+
 %% plot error
 figure('name',name); title error; hold on;
 plot(it1,err_it1, 'color','r','lineWidth', 2, 'marker','.');
